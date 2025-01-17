664cad19635fb3bf327496086b42d98d0e726d44
peternara <peternara@users.noreply.github.com>
2025-01-17 10:05:44 +0900

    Update ddpm.py

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -616,7 +616,10 @@ class LatentDiffusion(DDPM):
         else:
             self.register_buffer("scale_factor", torch.tensor(scale_factor))
         self.instantiate_first_stage(first_stage_config)
+
+        # PDE와 달리 condition 부분이 없는 알고리즘이므로 제거
         # self.instantiate_cond_stage(cond_stage_config)
+        
         self.cond_stage_forward = cond_stage_forward
         self.clip_denoised = False
         self.bbox_tokenizer = None
